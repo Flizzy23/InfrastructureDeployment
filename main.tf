a551a5aff87e56981354bd43867d8a23bb87a51f
Flizzy23 <adewalefawas01@gmail.com>
2025-10-02 18:43:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "adewale-jenkins"
+    bucket = "yusuf-jenkins"
     key    = "newresource.tfstate"
     region = "us-east-1"
   }
@@ -113,4 +113,5 @@ resource "aws_instance" "foo" {
 
 
 }
+
 
